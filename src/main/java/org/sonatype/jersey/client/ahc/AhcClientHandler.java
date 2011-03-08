84b593d115db2924f85370ea13b0a00c7065ab92
jfarcand <jfarcand@apache.org>
2011-03-08 14:49:33 -0500

    First drop

---
+++ src/main/java/org/sonatype/jersey/client/ahc/AhcClientHandler.java
@@ -0,0 +1,202 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2011 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * The Apache License v2.0 is available at
+ *   http://www.apache.org/licenses/LICENSE-2.0.html
+ * You may elect to redistribute this code under either of these licenses.
+ *******************************************************************************/
+package org.sonatype.jersey.client.ahc;
+
+import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.Cookie;
+import com.ning.http.client.FluentCaseInsensitiveStringsMap;
+import com.ning.http.client.RequestBuilder;
+import com.ning.http.client.Response;
+import com.sun.jersey.api.client.ClientHandler;
+import com.sun.jersey.api.client.ClientHandlerException;
+import com.sun.jersey.api.client.ClientRequest;
+import com.sun.jersey.api.client.ClientResponse;
+import org.sonatype.jersey.client.ahc.config.AhcConfig;
+import org.sonatype.jersey.client.ahc.config.DefaultAhcConfig;
+import com.sun.jersey.core.header.InBoundHeaders;
+import com.sun.jersey.spi.MessageBodyWorkers;
+
+import javax.ws.rs.core.Context;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * A root handler with Sonatype AsyncHttpClient acting as a backend.
+ * <p/>
+ * Client operations are thread safe, the HTTP connection may
+ * be shared between different threads.
+ * <p/>
+ * If a response entity is obtained that is an instance of {@link java.io.Closeable}
+ * then the instance MUST be closed after processing the entity to release
+ * connection-based resources.
+ * <p/>
+ * If a {@link ClientResponse} is obtained and an entity is not read from the
+ * response then {@link ClientResponse#close() } MUST be called after processing
+ * the response to release connection-based resources.
+ * <p/>
+ * The following methods are currently supported: HEAD, GET, POST, PUT, DELETE, TRACE
+ * OPTIONS as well as custom methods.
+ * <p/>
+ *
+ * @author Jeanfrancois Arcand
+ */
+public final class AhcClientHandler implements ClientHandler {
+
+    private final AsyncHttpClient client;
+
+    private final AhcConfig config;
+
+    private final AhcRequestWriter requestWriter = new AhcRequestWriter();
+
+    private List<Cookie> cookies = new ArrayList<Cookie>();
+
+    @Context
+    private MessageBodyWorkers workers;
+
+    /**
+     * Create a new root handler with an {@link AsyncHttpClient}.
+     *
+     * @param client the {@link AsyncHttpClient}.
+     */
+    public AhcClientHandler(AsyncHttpClient client) {
+        this(client, new DefaultAhcConfig());
+    }
+
+    /**
+     * Create a new root handler with an {@link AsyncHttpClient}.
+     *
+     * @param client the {@link AsyncHttpClient}.
+     * @param config the client configuration.
+     */
+    public AhcClientHandler(AsyncHttpClient client, AhcConfig config) {
+        this.client = client;
+        this.config = config;
+    }
+
+    /**
+     * Get the client config.
+     *
+     * @return the client config.
+     */
+    public AhcConfig getConfig() {
+        return config;
+    }
+
+    /**
+     * Get the {@link AsyncHttpClient}.
+     *
+     * @return the {@link AsyncHttpClient}.
+     */
+    public AsyncHttpClient getHttpClient() {
+        return client;
+    }
+
+    /**
+     * Translate the {@link ClientRequest} into a AsyncHttpClient request, and execute it.
+     * @param cr the HTTP request.
+     * @return the {@link ClientResponse}
+     * @throws ClientHandlerException
+     */
+    @Override
+    public ClientResponse handle(final ClientRequest cr)
+            throws ClientHandlerException {
+
+        try {
+            final RequestBuilder requestBuilder = getRequestBuilder(cr);
+            handleCookie(requestBuilder);
+            requestWriter.configureRequest(requestBuilder, cr, allowBody(cr.getMethod()));
+
+            final Response response = client.executeRequest(requestBuilder.build()).get();
+
+            cookies = response.getCookies();
+
+            ClientResponse r = new ClientResponse(response.getStatusCode(),
+                    getInBoundHeaders(response),
+                    response.getResponseBodyAsStream(),
+                    workers);
+            if (!r.hasEntity()) {
+                r.bufferEntity();
+                r.close();
+            }
+            return r;
+        } catch (Exception e) {
+            throw new ClientHandlerException(e);
+        }
+    }
+
+    /**
+     * Check if a body needs to be constructed based on a method's name.
+     * @param method An HTTP method
+     * @return true if s body can be allowed.
+     */
+    private boolean allowBody(String method) {
+        if ( method.equalsIgnoreCase("GET") || method.equalsIgnoreCase("OPTIONS")
+                    && method.equalsIgnoreCase("TRACE")
+                    && method.equalsIgnoreCase("HEAD")) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Return the {@link RequestBuilder} based on a method 
+     * @param cr the HTTP request.
+     * @return {@link RequestBuilder}
+     */
+    private RequestBuilder getRequestBuilder(ClientRequest cr) {
+        final String strMethod = cr.getMethod();
+        final String uri = cr.getURI().toString();
+
+        if (strMethod.equals("GET")) {
+            return new RequestBuilder("GET").setUrl(uri);
+        } else if (strMethod.equals("POST")) {
+            return new RequestBuilder("POST").setUrl(uri);
+        } else if (strMethod.equals("PUT")) {
+            return new RequestBuilder("PUT").setUrl(uri);
+        } else if (strMethod.equals("DELETE")) {
+            return new RequestBuilder("DELETE").setUrl(uri);
+        } else if (strMethod.equals("HEAD")) {
+            return new RequestBuilder("HEAD").setUrl(uri);
+        } else if (strMethod.equals("OPTIONS")) {
+            return new RequestBuilder("OPTIONS").setUrl(uri);
+        } else {
+            return new RequestBuilder(strMethod).setUrl(uri);
+        }
+    }
+
+    private InBoundHeaders getInBoundHeaders(Response response) {
+        InBoundHeaders headers = new InBoundHeaders();
+        FluentCaseInsensitiveStringsMap respHeaders = response.getHeaders();
+        for (Map.Entry<String, List<String>> header : respHeaders) {
+            headers.put(header.getKey(), header.getValue());
+        }
+        return headers;
+    }
+
+    /**
+     * Return the instance of {@link com.sun.jersey.api.client.RequestWriter}. This instance will be injected
+     * within Jersey so it cannot be null.
+     * @return the instance of {@link com.sun.jersey.api.client.RequestWriter}.
+     */
+    public AhcRequestWriter getAhcRequestWriter(){
+        return requestWriter;
+    }
+
+    private void handleCookie(RequestBuilder requestBuilder) {
+        for (Cookie c : cookies) {
+            requestBuilder.addCookie(c);
+        }
+    }
+    
+}
---
+++ src/test/java/org/sonatype/jersey/client/ahc/tests/HttpMethodTest.java
@@ -0,0 +1,291 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2011 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ * The Eclipse Public License is available at
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * The Apache License v2.0 is available at
+ *   http://www.apache.org/licenses/LICENSE-2.0.html
+ * You may elect to redistribute this code under either of these licenses.
+ *******************************************************************************/
+
+package org.sonatype.jersey.client.ahc.tests;
+
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.UniformInterfaceException;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.container.filter.LoggingFilter;
+import com.sun.jersey.api.core.DefaultResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+import org.sonatype.jersey.client.ahc.AhcHttpClient;
+import org.sonatype.jersey.client.ahc.config.AhcConfig;
+import org.sonatype.jersey.client.ahc.config.DefaultAhcConfig;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.HttpMethod;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author Paul.Sandoz@Sun.Com
+ */
+public class HttpMethodTest extends AbstractGrizzlyServerTester {
+    @Target({ElementType.METHOD})
+    @Retention(RetentionPolicy.RUNTIME)
+    @HttpMethod("PATCH")
+    public @interface PATCH {
+    }
+
+    @Path("/test")
+    public static class HttpMethodResource {
+        @GET
+        public String get() {
+            return "GET";
+        }
+
+        @POST
+        public String post(String entity) {
+            return entity;
+        }
+
+        @PUT
+        public String put(String entity) {
+            return entity;
+        }
+
+        @DELETE
+        public String delete() {
+            return "DELETE";
+        }
+
+        @DELETE
+        @Path("withentity")
+        public String delete(String entity) {
+            return entity;
+        }
+
+        @POST
+        @Path("noproduce")
+        public void postNoProduce(String entity) {
+        }
+
+        @POST
+        @Path("noconsumeproduce")
+        public void postNoConsumeProduce() {
+        }
+
+        @PATCH
+        public String patch(String entity) {
+            return entity;
+        }
+    }
+
+    public HttpMethodTest(String testName) {
+        super(testName);
+    }
+
+    protected AhcHttpClient createClient() {
+        return AhcHttpClient.create();
+    }
+
+    protected AhcHttpClient createClient(AhcConfig cc) {
+        return AhcHttpClient.create(cc);
+    }
+
+    public void testHead() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+        ClientResponse cr = r.head();
+        assertFalse(cr.hasEntity());
+    }
+
+    public void testOptions() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+        ClientResponse cr = r.options(ClientResponse.class);
+        assertTrue(cr.hasEntity());
+        cr.close();
+    }
+
+    public void testGet() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+        assertEquals("GET", r.get(String.class));
+
+        ClientResponse cr = r.get(ClientResponse.class);
+        assertTrue(cr.hasEntity());
+        cr.close();
+    }
+
+    public void testPost() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+        assertEquals("POST", r.post(String.class, "POST"));
+
+        ClientResponse cr = r.post(ClientResponse.class, "POST");
+        assertTrue(cr.hasEntity());
+        cr.close();
+    }
+
+    public void testPostChunked() {
+        ResourceConfig rc = new DefaultResourceConfig(HttpMethodResource.class);
+        rc.getProperties().put(ResourceConfig.PROPERTY_CONTAINER_REQUEST_FILTERS,
+                LoggingFilter.class.getName());
+        startServer(rc);
+
+        DefaultAhcConfig config = new DefaultAhcConfig();
+        config.getProperties().put(AhcConfig.PROPERTY_CHUNKED_ENCODING_SIZE, 1024);
+        AhcHttpClient c = createClient(config);
+
+        WebResource r = c.resource(getUri().path("test").build());        
+        assertEquals("POST", r.post(String.class, "POST"));
+
+        ClientResponse cr = r.post(ClientResponse.class, "POST");
+        assertTrue(cr.hasEntity());
+        cr.close();
+    }
+
+    public void testPostVoid() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+
+        // This test will lock up if ClientResponse is not closed by WebResource.
+        // TODO need a better way to detect this.
+        for (int i = 0; i < 100; i++) {
+            r.post("POST");
+        }
+    }
+
+    public void testPostNoProduce() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+        assertEquals(204, r.path("noproduce").post(ClientResponse.class, "POST").getStatus());
+
+        ClientResponse cr = r.path("noproduce").post(ClientResponse.class, "POST");
+        assertFalse(cr.hasEntity());
+        cr.close();
+    }
+
+    public void testPostNoConsumeProduce() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+        assertEquals(204, r.path("noconsumeproduce").post(ClientResponse.class).getStatus());
+
+        ClientResponse cr = r.path("noconsumeproduce").post(ClientResponse.class, "POST");
+        assertFalse(cr.hasEntity());
+        cr.close();
+    }
+
+    public void testPut() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+        assertEquals("PUT", r.put(String.class, "PUT"));
+
+        ClientResponse cr = r.put(ClientResponse.class, "PUT");
+        assertTrue(cr.hasEntity());
+        cr.close();
+    }
+
+    public void testDelete() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+        assertEquals("DELETE", r.delete(String.class));
+
+        ClientResponse cr = r.delete(ClientResponse.class);
+        assertTrue(cr.hasEntity());
+        cr.close();
+    }
+
+    public void testDeleteWithEntity() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test/withentity").build());
+        r.addFilter(new com.sun.jersey.api.client.filter.LoggingFilter());
+        assertEquals("DELETE with entity", r.delete(String.class, "DELETE with entity"));
+
+        ClientResponse cr = r.delete(ClientResponse.class, "DELETE with entity");
+        assertTrue(cr.hasEntity());
+        cr.close();
+    }
+
+    public void testPatch() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+        r.addFilter(new com.sun.jersey.api.client.filter.LoggingFilter());
+        assertEquals("PATCH", r.method("PATCH", String.class, "PATCH"));
+
+        ClientResponse cr = r.method("PATCH", ClientResponse.class, "PATCH");
+        assertTrue(cr.hasEntity());
+        cr.close();
+    }
+
+    public void testAll() {
+        startServer(HttpMethodResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+
+        assertEquals("GET", r.get(String.class));
+
+        assertEquals("POST", r.post(String.class, "POST"));
+
+        assertEquals(204, r.path("noproduce").post(ClientResponse.class, "POST").getStatus());
+
+        assertEquals(204, r.path("noconsumeproduce").post(ClientResponse.class).getStatus());
+
+        assertEquals("PUT", r.post(String.class, "PUT"));
+
+        assertEquals("DELETE", r.delete(String.class));
+    }
+
+
+    @Path("/test")
+    public static class ErrorResource {
+        @POST
+        public Response post(String entity) {
+            return Response.serverError().build();
+        }
+
+        @Path("entity")
+        @POST
+        public Response postWithEntity(String entity) {
+            return Response.serverError().entity("error").build();
+        }
+    }
+
+    public void testPostError() {
+        startServer(ErrorResource.class);
+        WebResource r = createClient().resource(getUri().path("test").build());
+
+        // This test will lock up if ClientResponse is not closed by WebResource.
+        // TODO need a better way to detect this.
+        for (int i = 0; i < 100; i++) {
+            try {
+                r.post("POST");
+            } catch (UniformInterfaceException ex) {
+            }
+        }
+    }
+
+    public void testPostErrorWithEntity() {
+        startServer(ErrorResource.class);
+        WebResource r = createClient().resource(getUri().path("test/entity").build());
+
+        // This test will lock up if ClientResponse is not closed by WebResource.
+        // TODO need a better way to detect this.
+        for (int i = 0; i < 100; i++) {
+            try {
+                r.post("POST");
+            } catch (UniformInterfaceException ex) {
+                String s = ex.getResponse().getEntity(String.class);
+                assertEquals("error", s);
+            }
+        }
+    }
+}
